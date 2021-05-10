1852eebf782605cd7992c3d3db244e7c709f58a9
brianars94 <brianars@hawaii.edu>
2021-05-09 20:29:38 -0500

    added a meaning component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import Results from "./Results";
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
 function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
 }
 
 function search(event) {
@@ -29,6 +31,7 @@ function handleKeywordChange(event) {
             <form onSubmit={search}>
                 <input type="search" onChange={handleKeywordChange} />
             </form>
+            <Results results={results} />
         </div>
     );
     }
---
+++ src/Meaning.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+export default function menaing(props){
+    return "hello from meaninggggg"
+}
