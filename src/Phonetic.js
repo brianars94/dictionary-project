b27a1efd6a94f22daba44e684f12480fb9020b01
brianars94 <brianars@hawaii.edu>
2021-05-22 17:47:31 -1000

    adding styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Phonetic(props) {
     return (
         <div className="Phonetic">
-            a href={props.phonetic.audio} target="_blank">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
             Listen Here!
             </a>
             <br />
             {props.phonetic.text}
-            
+
         </div>
     );
 }
